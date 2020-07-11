f4c9d3abcd48a7a043f408f18f14bf9977e93449
Dhruv Kalia <dhruvkalia13@gmail.com>
2020-07-11 23:33:59 +0530

    Day 1 | Importing Dataset

--- 100Days_daily_log.md
+++ 100Days_daily_log.md
@@ -1,1 +1,6 @@
 # Daily progress in #100DaysOfMLCode challenge
+
+## Day 1
+
+* Importing libraries
+* Importing dataset
---
+++ 1_Data_Preprocessing/import_dataset.py
@@ -0,0 +1,8 @@
+# Importing the libraries
+import pandas as pd
+
+# Importing data through pandas
+my_data = pd.read_csv("../1_Data_Preprocessing/Dataset/sample_data.csv")
+
+# Printing first 5 rows
+print(my_data.head())
