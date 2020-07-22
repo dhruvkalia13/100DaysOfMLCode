bbabc1b59c77a0597042b21350243962c0d59f02
Dhruv Kalia <dhruvkalia13@gmail.com>
2020-07-23 03:08:25 +0530

    Day 12 | K Nearest Neighbour

---
+++ 3_Classification/K_NearestNeighbour/knn.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
+
+# Importing dataset
+df = pd.read_csv("Dataset/tele_customer.csv")
+
+# Dependent and Indepent variables
+X = df[['region', 'tenure', 'age', 'marital', 'address', 'income', 'ed', 'employ', 'retire', 'gender', 'reside']]
+y = df['custcat']
+
+missing_cols = [col for col in df.columns if df[col].isnull().any()]
+print(missing_cols)
+
+# Feature Scaling
+X = preprocessing.StandardScaler().fit(X).transform(X.astype(float))
+
+# Splitting training and testing dataset
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=4)
+
+# Modelling with k from 1 to 6 and then storing the accuracy in mean_acc and std_acc
+Ks = 6
+mean_acc = np.zeros((Ks - 1))
+std_acc = np.zeros((Ks - 1))
+for n in range(1, Ks):
+    # Train Model
+    neigh = KNeighborsClassifier(n_neighbors=n).fit(X_train, y_train)
+    yhat = neigh.predict(X_test)
+    mean_acc[n - 1] = metrics.accuracy_score(y_test, yhat)
+    std_acc[n - 1] = np.std(yhat == y_test) / np.sqrt(yhat.shape[0])
+
+print(mean_acc)
+
+# Plot  model accuracy for different number of Neighbours
+plt.plot(range(1, Ks), mean_acc, 'g')
+plt.fill_between(range(1, Ks), mean_acc - 1 * std_acc, mean_acc + 1 * std_acc, alpha=0.10)
+plt.legend(('Accuracy ', '+/- 3xstd'))
+plt.ylabel('Accuracy ')
+plt.xlabel('Number of Neighbours (K)')
+plt.tight_layout()
+plt.show()
+
+print("The best accuracy was with", mean_acc.max(), "with k=", mean_acc.argmax() + 1)
