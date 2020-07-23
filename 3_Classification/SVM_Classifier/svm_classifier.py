92e17f7031f6fe02e9d4e59b1e7c67b57d9cd219
Dhruv Kalia <dhruvkalia13@gmail.com>
2020-07-24 04:24:42 +0530

    Day 13 | SVM Classifier

---
+++ 3_Classification/SVM_Classifier/svm_classifier.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import LinearSVC
+from sklearn.metrics import classification_report
+import matplotlib.pyplot as plt
+from sklearn.metrics import plot_confusion_matrix
+
+# Importing dataset
+df = pd.read_csv("Dataset/iris.csv")
+
+# Checking if data contains missing values
+missing_cols = [col for col in df.columns if df[col].isnull().any()]
+print(missing_cols)
+
+# Dependent and Independent variables
+X = df.iloc[:, [3, 4]]
+y = df.iloc[:, [5]]
+
+# Label encoding in categorical columns
+labelled_y = y.copy()
+label_encoder = LabelEncoder()
+for col in y.columns:
+    labelled_y[col] = label_encoder.fit_transform(y[col])
+
+# Splitting training and testing dataset
+X_train, X_test, y_train, y_test = train_test_split(X, labelled_y, test_size=0.2, random_state=42)
+
+# Pipeline to perform feature scaling and modelling
+svm_pipeline = Pipeline([
+    ("scaler", StandardScaler()),
+    ("linear_svc", LinearSVC(C=1, loss="hinge")),
+])
+svm_pipeline.fit(X_train, y_train)
+
+# Prediction and accuracy measurement
+y_test_hat = svm_pipeline.predict(X_test)
+print(classification_report(y_test, y_test_hat))
+print(plot_confusion_matrix(svm_pipeline, X_test, y_test, cmap=plt.cm.Blues, display_labels=['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']))
