6b5daaa20a6d4aa709b7c5f16a90851f1bbbe9e6
Dhruv Kalia <dhruvkalia13@gmail.com>
2020-07-28 22:47:58 +0530

    Day 18 | K Means Clustering

---
+++ 4_Clustering/K_Means_Clustering/k_means_clustering.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+
+# Importing dataset
+df = pd.read_csv("Dataset/sales.csv")
+X = df.iloc[:, 1:53]
+X.head()
+
+# Feature Scaling
+scaler = StandardScaler()
+scaled_X = pd.DataFrame(scaler.fit_transform(X))
+scaled_X.columns = X.columns
+
+# Using the elbow method to find the optimal number of clusters
+wcss = []
+for i in range(1, 9):
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 42)
+    kmeans.fit(scaled_X)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1, 9), wcss)
+plt.title('The Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+# Fitting K-Means to the dataset
+kmeans = KMeans(n_clusters = 3, init = 'k-means++', random_state = 42)
+y_kmeans = kmeans.fit_predict(scaled_X)
+
+# Visualising the clusters
+plt.scatter(scaled_X[y_kmeans == 0]['W0'], scaled_X[y_kmeans == 0]['W2'], s = 20, c = 'red', label = 'Cluster 1')
+plt.scatter(scaled_X[y_kmeans == 1]['W0'], scaled_X[y_kmeans == 1]['W2'], s = 20, c = 'green', label = 'Cluster 2')
+plt.scatter(scaled_X[y_kmeans == 2]['W0'], scaled_X[y_kmeans == 2]['W2'], s = 20, c = 'blue', label = 'Cluster 3')
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 30, c = 'yellow', label = 'Centroids')
+plt.title('Clusters of sales')
+plt.xlabel('First week')
+plt.ylabel('Third week')
+plt.legend()
+plt.show()
