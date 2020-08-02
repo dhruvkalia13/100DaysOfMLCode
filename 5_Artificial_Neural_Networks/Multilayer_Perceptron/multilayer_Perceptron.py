3ee8b2a5eaeb863417a8efc8f8908b1a9927fc7e
Dhruv Kalia <dhruvkalia13@gmail.com>
2020-08-03 04:03:36 +0530

    Day 23 | Classification MLP

--- 5_Artificial_Neural_Networks/Multilayer_Perceptron/multilayer_Perceptron.py
+++ 5_Artificial_Neural_Networks/Multilayer_Perceptron/multilayer_Perceptron.py
@@ -51,8 +51,6 @@ model = Sequential([
     layers.MaxPooling2D(),
     layers.Conv2D(32, 3, padding='same', activation='relu'),
     layers.MaxPooling2D(),
-    layers.Conv2D(64, 3, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
     layers.Flatten(),
     layers.Dense(128, activation='relu'),
     layers.Dense(num_classes)
@@ -65,7 +63,7 @@ model.compile(optimizer='adam',
 
 model.summary()
 
-epochs = 10
+epochs = 2
 history = model.fit(
     train_ds,
     validation_data=val_ds,
