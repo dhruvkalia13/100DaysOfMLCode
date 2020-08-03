d522e86e849b5b45610ba06053aafaeb23d383b4
Dhruv Kalia <dhruvkalia13@gmail.com>
2020-08-04 03:17:03 +0530

    Day 24 | Making predictions through ANN

--- 5_Artificial_Neural_Networks/Multilayer_Perceptron/multilayer_Perceptron.py
+++ 5_Artificial_Neural_Networks/Multilayer_Perceptron/multilayer_Perceptron.py
@@ -1,26 +1,30 @@
-import tensorflow as tf
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 from tensorflow.keras import layers
 import numpy as np
 from tensorflow.python.keras.models import Sequential
 import matplotlib.pyplot as plt
+import PIL
 
+import tensorflow as tf
 # training and validation data
 batch_size = 32
 img_height = 150
 img_width = 150
-data_dir = "Dataset/seg_train/"
+train_data_dir = "../input/intel-image-classification/seg_train/seg_train/"
 train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-    data_dir,
-    validation_split=0.2,
-    subset="training",
+    train_data_dir,
     seed=123,
     image_size=(img_height, img_width),
     batch_size=batch_size)
 
-val_ds = tf.keras.preprocessing.image_dataset_from_directory(
-    data_dir,
-    validation_split=0.2,
-    subset="validation",
+
+batch_size = 32
+img_height = 150
+img_width = 150
+test_data_dir = "../input/intel-image-classification/seg_test/seg_test/"
+test_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    test_data_dir,
     seed=123,
     image_size=(img_height, img_width),
     batch_size=batch_size)
@@ -33,6 +37,15 @@ for image_batch, labels_batch in train_ds:
     print(image_batch.shape)
     print(labels_batch.shape)
     break
+
+plt.figure(figsize=(10, 10))
+for images, labels in train_ds.take(1):
+  for i in range(6):
+    ax = plt.subplot(3, 3, i + 1)
+    plt.imshow(images[i].numpy().astype("uint8"))
+    plt.title(class_names[labels[i]])
+    plt.axis("off")
+
 
 # feature scaling
 normalization_layer = layers.experimental.preprocessing.Rescaling(1. / 255)
@@ -63,10 +76,10 @@ model.compile(optimizer='adam',
 
 model.summary()
 
-epochs = 2
+epochs = 10
 history = model.fit(
     train_ds,
-    validation_data=val_ds,
+    validation_data=test_ds,
     epochs=epochs
 )
 
@@ -91,3 +104,23 @@ plt.plot(epochs_range, val_loss, label='Validation Loss')
 plt.legend(loc='upper right')
 plt.title('Training and Validation Loss')
 plt.show()
+
+# Evaluating the model
+model.evaluate(test_ds)
+
+PIL.Image.open("../input/intel-image-classification/seg_pred/seg_pred/10013.jpg")
+PIL.Image.open("../input/intel-image-classification/seg_pred/seg_pred/10017.jpg")
+PIL.Image.open("../input/intel-image-classification/seg_pred/seg_pred/10021.jpg")
+image_paths = ['10013.jpg', '10017.jpg', '10021.jpg']
+
+for image_path in image_paths:
+    root_path = "../input/intel-image-classification/seg_pred/seg_pred/"
+    path = root_path + image_path
+    image = tf.keras.preprocessing.image.load_img(path)
+    input_arr = keras.preprocessing.image.img_to_array(image)
+    input_arr = np.array([input_arr])  # Convert single image to a batch.
+    score = tf.nn.softmax(model.predict(input_arr))
+    print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score)))
+
