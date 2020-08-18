42447766231e6ee627d2f11629b22a246957fb94
Dhruv Kalia <dhruvkalia13@gmail.com>
2020-08-19 04:07:14 +0530

    Day 39 | CNN (Fashion MNIST dataset)

---
+++ 6_Convolutional_Neural_Networks/fashion_mnist.py
@@ -0,0 +1,36 @@
+from tensorflow import keras
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(X_train_full, y_train_full), (X_test, y_test) = fashion_mnist.load_data()
+
+X_valid, X_train = X_train_full[:5000] / 255.0, X_train_full[5000:] / 255.0
+y_valid, y_train = y_train_full[:5000], y_train_full[5000:]
+
+class_names = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat",
+               "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
+
+X_train = X_train.reshape(55000, 28, 28, 1)
+X_valid = X_valid.reshape(5000, 28, 28, 1)
+
+model = keras.models.Sequential([
+    keras.layers.Conv2D(64, 7, activation="relu", padding="same", input_shape=(28, 28, 1)),
+    keras.layers.MaxPooling2D(2),
+    keras.layers.Conv2D(128, 3, activation="relu", padding="same"),
+    keras.layers.Conv2D(128, 3, activation="relu", padding="same"),
+    keras.layers.MaxPooling2D(2),
+    keras.layers.Conv2D(256, 3, activation="relu", padding="same"),
+    keras.layers.Conv2D(256, 3, activation="relu", padding="same"),
+    keras.layers.MaxPooling2D(2),
+    keras.layers.Flatten(),
+    keras.layers.Dense(128, activation="relu"),
+    keras.layers.Dropout(0.5),
+    keras.layers.Dense(64, activation="relu"),
+    keras.layers.Dropout(0.5),
+    keras.layers.Dense(10, activation="softmax"),
+])
+# Compiling the model
+opt = keras.optimizers.SGD(lr=0.01, momentum=0.9)
+model.compile(optimizer=opt, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+# fit model
+history = model.fit(X_train, y_train, epochs=10, batch_size=64, validation_data=(X_valid, y_valid))
